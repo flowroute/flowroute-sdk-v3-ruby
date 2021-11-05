bf6256b86e852bfe3b322786e7d89b6d7cd6476a
Carlos Garcia-Perez <carlos.garcia-perez@nikolamotor.com>
2021-11-05 14:00:50 -0700

    Add more robust check for Railtie logger

--- lib/flowroute_numbers_and_messaging/railtie.rb
+++ lib/flowroute_numbers_and_messaging/railtie.rb
@@ -5,7 +5,7 @@ require 'rails/railtie'
 module Lograge
   class Railtie < Rails::Railtie
     config.after_initialize do |app|
-      ::FlowrouteNumbersAndMessaging::Configuration.flowroute_logger = app.config.logger
+      ::FlowrouteNumbersAndMessaging::Configuration.flowroute_logger = Rails.logger if Rails.logger
     end
   end
 end
